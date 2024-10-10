ae263ce2ef795faba15fa327069d0b701031b905
Ilya <ilyawv@gmail.com>
2024-10-10 12:04:06 +0300

    Added a Backend for TF in AWS S3 with State locking with DynamoDB

---
+++ global/remote-backend/s3-backend.tf
@@ -0,0 +1,32 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket        = "bold-tf-state"
+  force_destroy = true
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
+  bucket = aws_s3_bucket.terraform_state.bucket
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
---
+++ global/remote-backend/variables.tf
@@ -0,0 +1,10 @@
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "dynamodb_name" {
+  description = "Name of the DynamoDB table"
+  type        = string
+}
+
